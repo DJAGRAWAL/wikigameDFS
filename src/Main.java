a53343564ff35bfc48c3004e8e04586b8b493fe3
DJAgrawal <devanjay@gmail.com>
2023-03-05 15:41:44 -0500

    last old mac commit

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ import java.net.URL;
 import java.util.*;
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
+import java.util.HashMap;
 //concat
 //contains***
 //compareto
@@ -18,30 +20,52 @@ import java.util.HashSet;
 
 
 public class Main  {
+    public static boolean two = false;
     public static void main(String[] args) {
-        String start = "https://en.wikipedia.org/wiki/National_Football_League";
-        search = "https://en.wikipedia.org/wiki/Green_Bay_Packers";
-        HtmlRead(start,0);
-        if(path.containsKey(search)) {
-            String s = search;
-            ArrayList<String> result = new ArrayList<>();
-            while(s!=start) {
-                result.add(s);
-                s = path.get(s);
+        String start = "https://en.wikipedia.org/wiki/Chad";
+        String search = "https://en.wikipedia.org/wiki/Trist%C3%A3o_Vaz_Teixeira";
+        HtmlRead(start,0, search);
+        if(!path.containsKey(search)) {
+            two = true;
+            HtmlRead(search,0, start);
+            for(String s: path.keySet()) {
+                if(path2.values().contains(path.get(s))) {
+                    String cur = path.get(s);
+                    ArrayList<String> result = new ArrayList<>();
+                    while (!cur.equals(start)) {
+                        result.add(cur);
+                        cur = path.get(cur);
+                    }
+                    result.add(cur);
+                    Collections.reverse(result);
+                    cur = path2.get(path.get(s));
+
+                    while (!cur.equals(start)) {
+                        result.add(cur);
+                        cur = path2.get(cur);
+                    }
+                    for(String s2: result) System.out.println(s2);
+                    break;
+                }
             }
-            result.add(start);
-            Collections.reverse(result);
-            for(String s2: result) System.out.println(s2);
         }
         else {
-            System.out.println("NO PATH FOUND");
+            ArrayList<String> result = new ArrayList<String>();
+            String cur = search;
+            while (!cur.equals(start)) {
+                result.add(cur);
+                cur = path.get(cur);
+            }
+            result.add(cur);
+            Collections.reverse(result);
+            for(String s: result) System.out.println(s);
         }
     }
     public static HashMap<String, String> path = new HashMap<>(); // curlink to past link
+    public static HashMap<String, String> path2 = new HashMap<>();
     public static int MAXDEPTH = 3;
-    public static String search;
 
-    public static void HtmlRead(String link, int depth) {
+    public static void HtmlRead(String link, int depth, String search) {
         if(depth>=MAXDEPTH) return;
         try {
             URL url = new URL(link);
@@ -50,7 +74,7 @@ public class Main  {
             );
             String line;
             while ((line = reader.readLine()) != null) {
-                parseLine(line, link, depth);
+                parseLine(line, link, depth, search);
             }
         }
         catch(Exception e) {
@@ -59,9 +83,9 @@ public class Main  {
 
     }
     public static boolean run = true;
-    public static void parseLine(String line, String pastlink, int depth) {
+    public static void parseLine(String line, String pastlink, int depth, String search) {
         if(!run) return;
-        String[] starters = new String[]{"href='","href=\"", "src='", "src=\""};
+        String[] starters = new String[]{"href='","href=\""};
         for(String s: starters) {
             if (line.contains(s)) {
 //                System.out.println(line);
@@ -81,15 +105,17 @@ public class Main  {
                 link = "https://en.wikipedia.org" + link;
                 if(link.equals(search)) {
 //                    System.out.println("FOUND");
-                    path.put(link, pastlink);
+                    if(two) path2.put(link, pastlink);
+                    else path.put(link, pastlink);
                     run = false;
                     return;
                 }
 //                Depth First:
                 if(!path.containsKey(link)) {
 //                    System.out.println(link);
-                    path.put(link, pastlink);
-                    HtmlRead(link, depth+1);
+                    if(two) path2.put(link, pastlink);
+                    else path.put(link, pastlink);
+                    HtmlRead(link, depth+1, search);
                 }
 //                if(bottom.getText().contains(link)) {
 //                    bottom.append(link + "\n\n");
@@ -97,7 +123,7 @@ public class Main  {
 //                }
                 if(end==x.length) return;
                 line = line.substring(end+1);
-                parseLine(line, pastlink, depth);
+                parseLine(line, pastlink, depth, search);
                 break;
             }
 
